f1e3c8318658c9f7cf1edcdec6419e32a622be7e
Daniel <dhonaruto@gmail.com>
2022-06-21 23:29:44 -0400

    Attempted to solve problem Ic340.js #5

--- Students/danielHo/SlidingWindow/lc340.js
+++ Students/danielHo/SlidingWindow/lc340.js
@@ -6,6 +6,36 @@
 
 function longest_substring_with_k_distinct(str, k) {
   // TODO: Write code here
+  if (k == 0) return 0
+
+  // start and end pointers
+  let start = 0;
+  let end = 0;
+
+  // Storing the characters
+  let dict = {}
+
+  let maxLength = 0;
+
+  while (end < str.length) {
+    if (dict.hasOwnProperty(str[end])) dict[str[end]]++;
+    else dict[str[end]] = 1;
+    console.log()
+
+    while (Object.keys(dict) > k) {
+      dict[str[start]]--;
+
+      if (dict[str[start]] == 0) {
+        delete dict[str[start]]
+      }
+      start++;
+    }
+
+    maxLength = Math.max(maxLength, end - start + 1)
+    end++;
+  }
+  
+  return maxLength;
 }
 
 console.log(
